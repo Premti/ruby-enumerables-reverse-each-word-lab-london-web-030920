3bfe7d5939c5b2340cdcf76f404fe455efdbf448
premti <premti@gmail.com>
2020-03-04 11:00:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   array = string.split(" ")
   reversed_strings = []
-  array.collect do |string|
+  array.each do |string|
     reversed_strings.push("#{string.reverse}")
  end
  reversed_strings
