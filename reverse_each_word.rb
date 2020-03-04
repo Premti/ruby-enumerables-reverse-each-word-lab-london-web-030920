1227f849a494e0114c15a2b82b90232f27bdade6
premti <premti@gmail.com>
2020-03-04 10:40:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,9 @@
 def reverse_each_word(string)
   array = string.split
+  new_array = []
   array.each do |n|
     array = "#{array.reverse}"
-    array = array.join
+    new_array = array.join
  end
  array
 end
